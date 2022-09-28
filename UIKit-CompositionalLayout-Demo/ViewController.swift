34d09ac27aa9b22d4e37bac95bce6bd2505394c4
Yılmaz Edis <ylmazedis@gmail.com>
2022-09-28 15:46:00 +0300

    added collection view paging layout with animation

--- UIKit-CompositionalLayout-Demo/ViewController.swift
+++ UIKit-CompositionalLayout-Demo/ViewController.swift
@@ -9,31 +9,81 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var collectionView: UICollectionView = {
-        let view = UICollectionView(frame: .zero, collectionViewLayout: getCompositionalLayout())
-        view.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
-        view.setCollectionViewLayout(getCompositionalLayout(), animated: false)
-        view.backgroundColor = .white
+    lazy var pagingCollectionView: UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: getPagingCompositionalLayout())
+        view.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "pagingCollectionViewCell")
+        return view
+    }()
+    
+    lazy var groupCollectionView: UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: getGroupCompositionalLayout())
+        view.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "groupCollectionViewCell")
         return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCollectionView()
+        setupLayout()
     }
     
     func setupCollectionView() {
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        view.addSubview(collectionView)
+        pagingCollectionView.delegate = self
+        pagingCollectionView.dataSource = self
+        
+        groupCollectionView.delegate = self
+        groupCollectionView.dataSource = self
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        collectionView.frame = view.bounds
+    private func setupLayout() {
+        setPagingCollectionViewLayout()
+        setGroupCollectionViewLayout()
     }
     
-    private func getCompositionalLayout() -> UICollectionViewCompositionalLayout {
+    private func setPagingCollectionViewLayout() {
+        view.addSubview(pagingCollectionView)
+        pagingCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        pagingCollectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        pagingCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        pagingCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        pagingCollectionView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+    
+    private func setGroupCollectionViewLayout() {
+        view.addSubview(groupCollectionView)
+        groupCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        groupCollectionView.topAnchor.constraint(equalTo: pagingCollectionView.bottomAnchor, constant: 20).isActive = true
+        groupCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        groupCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        groupCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    private func getPagingCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        // --------- Carousel ---------                                           // make fraction 1 if needs no whitespace
+        let carouselItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.95),
+                                                                                     heightDimension: .absolute(200)))
+        carouselItem.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+
+        let carouselGroup = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(200)), subitems: [carouselItem])
+
+        let carouselSection = NSCollectionLayoutSection(group: carouselGroup)
+        carouselSection.orthogonalScrollingBehavior = .groupPagingCentered
+        
+        // animation
+        carouselSection.visibleItemsInvalidationHandler = { (items, offset, environment) in
+            items.forEach { item in
+                let distanceFromCenter = abs((item.frame.midX - offset.x) - environment.container.contentSize.width / 2.0)
+                let minScale: CGFloat = 0.7
+                let maxScale: CGFloat = 1.1
+                let scale = max(maxScale - (distanceFromCenter / environment.container.contentSize.width), minScale)
+                item.transform = CGAffineTransform(scaleX: scale, y: scale)
+            }
+        }
+        let layout = UICollectionViewCompositionalLayout(section: carouselSection)
+        return layout
+    }
+    
+    private func getGroupCompositionalLayout() -> UICollectionViewCompositionalLayout {
         
         let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)))
         item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
@@ -48,9 +98,9 @@ class ViewController: UIViewController {
 
          let nestedGroup2Item1 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/2), heightDimension: .fractionalHeight(1)))
         nestedGroup2Item1.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
-        
+
         let nestedGroup2 = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/2)), subitems: [nestedGroup2Item1])
-        
+
         let nestedGroup1 = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/2), heightDimension: .fractionalHeight(1)), subitems: [nestedGroup1Item1, nestedGroup2])
 
         let group1 = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)), subitems: [group1Item1, nestedGroup1])
@@ -67,46 +117,36 @@ class ViewController: UIViewController {
         let section = NSCollectionLayoutSection(group: containerGroup)
         let layout = UICollectionViewCompositionalLayout(section: section)
         return layout
-        
-        /*
-         //--------- Carousel ---------//
-        let carouselItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.95), heightDimension: .absolute(200)))
-        carouselItem.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
-        
-        let carouselGroup = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(200)), subitems: [caraousalItem])
-        
-        let carouselSection = NSCollectionLayoutSection(group: carouselGroup)
-        carouselSection.orthogonalScrollingBehavior = .paging
-        
-        carouselSection.visibleItemsInvalidationHandler = { (items, offset, environment) in
-            items.forEach { item in
-                let distanceFromCenter = abs((item.frame.midX - offset.x) - environment.container.contentSize.width / 2.0)
-                let minScale: CGFloat = 0.7
-                let maxScale: CGFloat = 1.1
-                let scale = max(maxScale - (distanceFromCenter / environment.container.contentSize.width), minScale)
-                item.transform = CGAffineTransform(scaleX: scale, y: scale)
-            }
-        }
-        let layout = UICollectionViewCompositionalLayout(section: carouselSection)
-        return layout
-         */
     }
 }
 
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 70
+        if collectionView == pagingCollectionView {
+            return 20
+        } else {
+            return 70
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
-        let imageView = UIImageView(frame: cell.frame)
-        imageView.image = UIImage(named: "\(indexPath.row + 1)")
-        imageView.contentMode = .scaleToFill
-        //cell.add(view: imageView, left: 0, right: 0, top: 0, bottom: 0)
-        cell.backgroundColor = .random
-        return cell
+        
+        if collectionView == pagingCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pagingCollectionViewCell", for: indexPath)
+            let imageView = UIImageView(frame: cell.frame)
+            imageView.image = UIImage(named: "\(indexPath.row + 1)")
+            imageView.contentMode = .scaleToFill
+            cell.backgroundColor = .random
+            return cell
+        } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "groupCollectionViewCell", for: indexPath)
+            let imageView = UIImageView(frame: cell.frame)
+            imageView.image = UIImage(named: "\(indexPath.row + 1)")
+            imageView.contentMode = .scaleToFill
+            cell.backgroundColor = .random
+            return cell
+        }
     }
 }
 
@@ -116,20 +156,5 @@ extension UIColor {
                        green: .random(in: 0.4...1),
                        blue: .random(in: 0.4...1),
                        alpha: 1)
-    }
-}
-
-extension UIView {
-    func add(view: UIView, left: CGFloat = 0, right: CGFloat = 0, top: CGFloat = 0, bottom: CGFloat = 0) {
-        
-        view.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(view)
-        
-        view.leftAnchor.constraint(equalTo: self.leftAnchor, constant: left).isActive = true
-        view.rightAnchor.constraint(equalTo: self.rightAnchor, constant: right).isActive = true
-        
-        view.topAnchor.constraint(equalTo: self.topAnchor, constant: top).isActive = true
-        view.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: bottom).isActive = true
-        
     }
 }
